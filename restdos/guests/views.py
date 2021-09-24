79df4a2cbf4192d3eebf87b1122c3fdf3e9fcc30
sreninc <sreninc@gmail.com>
2021-09-24 17:41:58 +0000

    FEAT: Updated guests templates and app base template

--- restdos/guests/views.py
+++ restdos/guests/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def guests(request):
+    return render(request, 'guests/guests.html')
--- restdos/restdos/settings.py
+++ restdos/restdos/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'website',
+    'guests',
 ]
 
 MIDDLEWARE = [
