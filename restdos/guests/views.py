6abc879bed0c4daefbd8cce4ca2697452e0f9a76
sreninc <sreninc@gmail.com>
2021-09-25 12:08:48 +0000

    FEAT: Added guest search to base-app

--- restdos/guests/views.py
+++ restdos/guests/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Guest
 
 # Create your views here.
 def guests(request):
+
     guests = Guest.objects.all()
+    query = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('guests'))
+
+            queries = Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            guests = guests.filter(queries)
+
+
+    
 
     for guest in guests:
         guest.unrating = 5 - guest.rating
@@ -12,6 +29,7 @@ def guests(request):
 
     context = {
         'guests': guests,
+        'search_term': query,
     }
 
     return render(request, 'guests/guests.html', context)
