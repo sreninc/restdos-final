cb8fe18447d57b06ed3ac62dca15ecec3bd8eb57
sreninc <sreninc@gmail.com>
2021-09-25 08:34:27 +0000

    FEAT: Added guest fixtures and related files to show on guests.html

--- restdos/guests/models.py
+++ restdos/guests/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Guest(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    mobile = models.CharField(max_length=15)
+    email = models.EmailField()
+    rating = models.IntegerField(default=5)
--- restdos/guests/views.py
+++ restdos/guests/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Guest
 
 # Create your views here.
 def guests(request):
-    return render(request, 'guests/guests.html')
+    guests = Guest.objects.all()
+
+    for guest in guests:
+        guest.unrating = 5 - guest.rating
+        guest.rating = range(guest.rating)
+        guest.unrating = range(guest.unrating)
+
+    context = {
+        'guests': guests,
+    }
+
+    return render(request, 'guests/guests.html', context)
