303c09d816853ee98757c70fde904f3df3e0d293
sreninc <sreninc@gmail.com>
2021-09-27 18:32:33 +0000

    FEAT: Added signup templates

--- restdos/signup/views.py
+++ restdos/signup/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def details(request):
+    return render(request, 'signup/details.html')
+
+
+def business(request):
+    return render(request, 'signup/business.html')
+
+
+def restaurant(request):
+    return render(request, 'signup/restaurant.html')
+
+
+def billing(request):
+    return render(request, 'signup/billing.html')
