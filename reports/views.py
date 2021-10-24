f3eb637f7e5c48fc9c524e9c25e96ff8d9b1e83e
sreninc <sreninc@gmail.com>
2021-10-24 16:46:06 +0000

    FEAT: Added code to reports/dashboard to generate values

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Sum
 
 from guests.models import Guest
 from bookings.models import Booking
@@ -10,13 +11,16 @@ def dashboard(request):
 
     guests = Guest.objects.all()
     bookings = Booking.objects.all()
+    print(bookings)
+    print(type(bookings.first().booking_value))
+    print(bookings.filter(status='CON'))
 
     total_guests = guests.count()
     total_bookings = bookings.count()
-    total_sales = 0
-    no_show_percentage = 0
-    completed_percentage = 0
-    avg_booking_value = 0
+    total_sales = bookings.filter(status='COM').aggregate(Sum('booking_value'))['booking_value__sum']
+    no_show_percentage = (bookings.filter(status='NOS').count() / bookings.count()) * 100
+    completed_percentage = (bookings.filter(status='COM').count() / bookings.count()) * 100
+    avg_booking_value = bookings.filter(status='COM').aggregate(Sum('booking_value'))['booking_value__sum'] / bookings.count()
 
     stats = {
         'total_guests': total_guests,
