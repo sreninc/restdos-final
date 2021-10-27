5e45c6e7d0df70b2165f209dd14daff241976d7a
sreninc <sreninc@gmail.com>
2021-10-27 07:16:17 +0000

    DEP: Updated secret key and debug to check environ

--- restdos/settings.py
+++ restdos/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hni3i(woxq0#u(ks$$xjgw+b(2+4n_686$&sjuh$mhodnh)mc!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['restdos-final.herokuapp.com', 'localhost']
 
