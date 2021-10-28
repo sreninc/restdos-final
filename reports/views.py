0dec949b30b547dd598af815b8e3f52909a34ab8
sreninc <sreninc@gmail.com>
2021-10-28 12:25:04 +0000

    FIX: Corrected object calls in reports/dashboard view

--- reports/views.py
+++ reports/views.py
@@ -9,8 +9,8 @@ from bookings.models import Booking
 # Create your views here.
 def dashboard(request):
 
-    guests = Guest.objects.all(deleted=False)
-    bookings = Booking.objects.all(deleted=False)
+    guests = Guest.objects.filter(deleted=False)
+    bookings = Booking.objects.filter(deleted=False)
 
     total_guests = 0
     total_bookings = 0
