bf7f80ce7fedffddd0896cf2346cbba2b22232bd
sreninc <sreninc@gmail.com>
2021-10-30 16:24:40 +0000

    FIX: Removed irrelevant fields from signup form and corrected logout trans

--- website/forms.py
+++ website/forms.py
@@ -4,16 +4,11 @@ from django import forms
 class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
-    mobile = forms.CharField(max_length=20, label='Mobile')
-    signup_plan = forms.CharField(max_length=30, label='Signup Plan')
-    signup_monthly = forms.IntegerField(label='Monthly Fee')
+
 
 
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.mobile = self.cleaned_data['mobile']
-        user.signup_plan = self.cleaned_data['signup_plan']
-        user.signup_monthly = self.cleaned_data['signup_monthly']
         user.save()
