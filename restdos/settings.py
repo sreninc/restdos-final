6894744ff79cd72213243a7285d37e94c881cb84
sreninc <sreninc@gmail.com>
2021-10-31 11:24:14 +0000

    FEAT: Added production email settings to settings2

--- restdos/settings.py
+++ restdos/settings.py
@@ -102,9 +102,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-SMS_BACKEND = 'sms.backends.console.SmsBackend' #required for django-sms
-
 TAILWIND_APP_NAME = 'theme' # required for django-tailwind
 
 INTERNAL_IPS = [
@@ -215,3 +212,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    SMS_BACKEND = 'sms.backends.console.SmsBackend' #required for django-sms
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
