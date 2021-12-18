588af06573af8529a1aac4a5d10329518ff9a4f1
sreninc <sreninc@gmail.com>
2021-12-18 12:36:01 +0000

    FEAT: Added date range filter to dashboard

--- reports/urls.py
+++ reports/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/<filter>', views.dashboard, name='dashboard'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
@@ -9,10 +10,19 @@ from bookings.models import Booking
 
 # Create your views here.
 @login_required
-def dashboard(request):
+def dashboard(request,
+              filter=7):
+
+    filter = int(filter)
+    start_date = datetime.now() - timedelta(days=filter)
+    start_date = start_date.strftime('%Y-%m-%d') 
+    end_date = datetime.now().strftime('%Y-%m-%d') 
 
     guests = Guest.objects.filter(deleted=False, user=request.user)
-    bookings = Booking.objects.filter(deleted=False, user=request.user)
+    bookings = Booking.objects.filter(
+        deleted=False,
+        user=request.user,
+        date__range=[start_date, end_date])
 
     total_guests = 0
     total_bookings = 0
@@ -35,6 +45,11 @@ def dashboard(request):
     if bookings.filter(status='NOS'):
         no_show_percentage = (bookings.filter(status='NOS').count() / bookings.count()) * 100
 
+    start_filter_date = datetime.now() - timedelta(days=filter)
+    start_filter_date = start_filter_date.strftime('%b %I')
+    end_filter_date = datetime.now().strftime('%b %I')
+    page_description = start_filter_date + ' - ' + end_filter_date
+
     stats = {
         'total_guests': total_guests,
         'total_bookings': total_bookings,
@@ -47,5 +62,7 @@ def dashboard(request):
     context = {
         'stats': stats,
         'page': 'dashboard',
+        'filter': filter,
+        'page_description': page_description,
     }
     return render(request, 'reports/dashboard.html', context)
